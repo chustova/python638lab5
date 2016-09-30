ea817d06f5791403512916a02e399fd6edcc256b
student <polinachekalkina@gmail.com>
2016-09-30 13:41:42 +0300

    Added .idea to gitignore Added test file

---
+++ test/test.py
@@ -0,0 +1,15 @@
+__author__ = 'student'
+
+primes = [2, 3, 5, 7, 11, 13]
+Rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+
+my_list = []
+
+for i in range (int(input())):
+    my_list.append(int(input()))
+
+print(my_list)
+
+
+
+
